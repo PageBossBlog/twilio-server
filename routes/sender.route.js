3d2b3ec0c3553cc3a48b3920d353be1bfed38581
PageBossBlog <151723753+PageBossBlog@users.noreply.github.com>
2024-01-24 02:43:30 +0100

    Update sender.route.js

--- routes/sender.route.js
+++ routes/sender.route.js
@@ -1,10 +1,13 @@
 import express from "express";
 import {
-  smsSender
+  smsSender,
+  smsSenderWithMessagingService
 } from "../controllers/sender.controller.js";
 
 const router = express.Router();
 
 router.post("/sms", smsSender);
+
+router.post("/messagingSid", smsSenderWithMessagingService);
 
 export default router;
